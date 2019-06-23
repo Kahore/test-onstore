bf70f06082b6c64f68a23a1ce0b02da931ee58a4
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-06-23 12:58:26 +0700

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  endOfLine: 'crlf',
+  spaceBeforeFunctionParen: true,
+};
